18bbd1fead30a5c7f6ff136fd09b1bbcfce46ba3
piotr biedermann <piotr.biedermann84@gmail.com>
2023-03-06 19:03:40 +0100

    working ex04

--- ex04/main.cpp
+++ ex04/main.cpp
@@ -1,17 +1,22 @@
 #include <iostream>
 #include <fstream>
 
-int main ()
+int main (int argc, char** argv)
 {
-    std::string     compareString = "poo";
-    std::string     inputString; //input file string here
-    std::string     replacementString = "starcraft";
+    if(argc < 4)
+    {
+        std::cout << "invalid number of arguments" << std::endl;
+        return 1;
+    }
+    std::string filename = argv[1];
+    std::string     compareString = argv[2];
+    std::string     inputString;
+    std::string     replacementString = argv[3];
     int             i = 0;
     int             compareCount = 0;
-    std::ifstream    inputFileStream("testInputFile.txt");
-    std::ofstream    outputFileStream("testOutputFile.txt");
-    while(getline(inputFileStream, inputString))
-        inputFileStream >> inputString;
+    std::ifstream    inputFileStream(filename);
+    std::ofstream    outputFileStream(filename + ".replace");
+    while(getline(inputFileStream, inputString)){}
     inputFileStream.close();
     while(i < inputString.length())
     {
@@ -19,13 +24,13 @@ int main ()
         {
             compareCount++;
             if (compareCount == compareString.length()) {
-                outputFileStream << replacementString; //output file here
+                outputFileStream << replacementString;
                 i = i + compareCount;
             }
         }
         if (compareString[compareCount] != inputString[i])
         {
-            outputFileStream << inputString[i]; //output file here
+            outputFileStream << inputString[i];
             compareCount = 0;
         }
         if (i < inputString.length())
